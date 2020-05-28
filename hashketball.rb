fd088fc84de0a284cb7af4594b77daa575f9f199
Lauren Frankovich <frankovister@gmail.com>
2020-05-28 22:19:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,12 +3,33 @@ def game_hash
     :home => {
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
-    :players => ["Alan Anderson", "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry"]
+    :players => [
+      "Alan Anderson" = {
+        :Number =>
+        :Shoe =>
+        :Points =>
+        :Rebounds =>
+        :Assists =>
+        :Steals =>
+        :Blocks =>
+        :Slam_dunks =>
+      }, 
+      "Reggie Evans", 
+      "Brook Lopez", 
+      "Mason Plumlee", 
+      "Jason Terry"
+      ]
     },
   :away => {
     :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
-    :players => ["Jeff Adrien", "Bismack Biyombo", "DeSagna Diop", "Ben Gordon", "Kemba Walker"]
+    :players => [
+      "Jeff Adrien", 
+      "Bismack Biyombo", 
+      "DeSagna Diop", 
+      "Ben Gordon", 
+      "Kemba Walker"
+      ]
   }
 }
 end
