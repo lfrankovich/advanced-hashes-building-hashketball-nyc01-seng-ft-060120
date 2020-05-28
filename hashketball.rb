3795f6dd2a4ff3aa0f623d4c17992267a0bc9225
Lauren Frankovich <frankovister@gmail.com>
2020-05-28 22:29:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -35,24 +35,24 @@ def game_hash
         :slam_dunks => 15
       }, 
       "Mason Plumlee"=> {
-        :number => ,
-        :shoe => ,
-        :points => ,
-        :rebounds => ,
-        :assists => ,
-        :steals => ,
-        :blocks => ,
-        :slam_dunks =>
+        :number => 1,
+        :shoe => 19,
+        :points => 26,
+        :rebounds => 11,
+        :assists => 6,
+        :steals => 3,
+        :blocks => 8,
+        :slam_dunks => 5
       },  
       "Jason Terry" => {
-        :number => ,
-        :shoe => ,
-        :points => ,
-        :rebounds => ,
-        :assists => ,
-        :steals => ,
-        :blocks => ,
-        :slam_dunks => 
+        :number => 31,
+        :shoe => 15,
+        :points => 19,
+        :rebounds => 2,
+        :assists => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_dunks => 1
       }
       ]
     },
@@ -61,44 +61,44 @@ def game_hash
     :colors => ["Turquoise", "Purple"],
     :players => [
       "Jeff Adrien"=> {
-        :number => ,
-        :shoe => ,
-        :points => ,
-        :rebounds => , 
-        :assists => ,
-        :steals => ,
-        :blocks => ,
-        :slam_dunks =>
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1, 
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2
       },  
       "Bismack Biyombo" => {
-        :number => ,
-        :shoe => ,
-        :points => , 
-        :rebounds => ,
-        :assists => ,
-        :steals => ,
-        :blocks => ,
-        :slam_dunks =>
+        :number => 0,
+        :shoe => 16,
+        :points => 12, 
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10
       }, 
       "DeSagna Diop" => {
-        :number => ,
-        :shoe => ,
-        :points => ,
-        :rebounds => ,
-        :assists => ,
-        :steals => ,
-        :blocks => , 
-        :slam_dunks =>
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5, 
+        :slam_dunks => 5
       }, 
       "Ben Gordon" => {
-        :number => ,
-        :shoe => ,
-        :points => ,
-        :rebounds => ,
-        :assists => ,
-        :steals => ,
-        :blocks => , 
-        :slam_dunks =>
+        :number => 8,
+        :shoe => 15,
+        :points => 33,
+        :rebounds => 3,
+        :assists => 2,
+        :steals => 1,
+        :blocks => 1, 
+        :slam_dunks => 0
       }, 
       "Kemba Walker" => {
         :number => ,
