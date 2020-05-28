6826788d566eecce0d602eaaa1b02775299f9fb0
Lauren Frankovich <frankovister@gmail.com>
2020-05-28 22:24:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,30 +5,111 @@ def game_hash
     :colors => ["Black", "White"],
     :players => [
       "Alan Anderson" => {
-        :number =>
-        :shoe =>
-        :points =>
-        :rebounds =>
-        :assists =>
-        :steals =>
-        :blocks =>
+        :number => "0",
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
         :slam_dunks =>
       }, 
-      "Reggie Evans", 
-      "Brook Lopez", 
-      "Mason Plumlee", 
-      "Jason Terry"
+      "Reggie Evans" => {
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => , 
+        :blocks => ,
+        :slam_dunks =>
+      }, 
+      "Brook Lopez" => {
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks =>
+      }, 
+      "Mason Plumlee"=> {
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks =>
+      },  
+      "Jason Terry" => {
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks => 
+      }
       ]
     },
   :away => {
     :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
     :players => [
-      "Jeff Adrien", 
-      "Bismack Biyombo", 
-      "DeSagna Diop", 
-      "Ben Gordon", 
-      "Kemba Walker"
+      "Jeff Adrien"=> {
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => , 
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks =>
+      },  
+      "Bismack Biyombo" => {
+        :number => ,
+        :shoe => ,
+        :points => , 
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks =>
+      }, 
+      "DeSagna Diop" => {
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => , 
+        :slam_dunks =>
+      }, 
+      "Ben Gordon" => {
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => , 
+        :slam_dunks =>
+      }, 
+      "Kemba Walker" => {
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks =>
+      }
       ]
   }
 }
